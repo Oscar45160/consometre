69e3d0af321a3d6f2818501fa0cf6acbe9d392ad
Oscar45160 <67793554+Oscar45160@users.noreply.github.com>
2023-04-04 09:13:44 +0000

    modele not working

--- predic_lm.py
+++ predic_lm.py
@@ -32,3 +32,38 @@ y_pred = model.predict(X_test)
 # Calculer l'erreur quadratique moyenne (RMSE) pour évaluer la performance du modèle
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 print("RMSE:", rmse)
+
+from statsmodels.tsa.arima.model import ARIMA
+
+# Charger les données historiques (remplacez par le nom de votre fichier CSV)
+weather_data = pd.read_csv("donnees.csv")
+
+# Convertir la colonne de date en un objet datetime
+weather_data["datehour"] = pd.to_datetime(weather_data["datehour"])
+
+# Supprimer les doublons
+weather_data = weather_data.drop_duplicates(subset="datehour")
+
+# Régler la fréquence de l'index de date (ajustez 'H' en fonction de la fréquence de vos données)
+weather_data = weather_data.set_index("datehour").asfreq('H')
+
+# Sélectionner les variables pertinentes
+temperature = weather_data[["Température (°C)"]]
+dew_point = weather_data[["Point de rosée"]]
+
+# Créer et ajuster les modèles ARIMA
+temperature_model = ARIMA(temperature, order=(1, 1, 1)).fit()
+dew_point_model = ARIMA(dew_point, order=(1, 1, 1)).fit()
+
+# Prévoir les valeurs des variables pertinentes pour le jour suivant
+temperature_forecast = temperature_model.forecast(steps=1)[0]
+dew_point_forecast = dew_point_model.forecast(steps=1)[0]
+
+
+temperature_next_day = temperature_forecast
+dew_point_next_day = dew_point_forecast
+datehour_next_day = 5
+datemonth_next_day = 1
+
+prediction = model.predict([[temperature_next_day, dew_point_next_day, datehour_next_day, datemonth_next_day]])
+print("Consommation électrique prévue pour le jour suivant :", prediction[0])
