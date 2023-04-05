c0793c93b6f7eac753c79996b7c40bc4e9bec94e
Oscar45160 <67793554+Oscar45160@users.noreply.github.com>
2023-04-05 19:38:29 +0000

    codespace

--- server.js
+++ server.js
@@ -75,3 +75,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
